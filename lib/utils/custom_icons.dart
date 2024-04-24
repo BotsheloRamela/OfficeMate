4dcc54d6e82370342419d34e9257a60c87db1b3f
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-04-24 22:54:38 +0200

    Create custom icons util class

---
+++ lib/utils/custom_icons.dart
@@ -0,0 +1,11 @@
+class CustomIcons {
+  static const String email = 'assets/icons/email.svg';
+  static const String phone = 'assets/icons/phone.svg';
+  static const String location = 'assets/icons/location.svg';
+  static const String edit = 'assets/icons/edit.svg';
+  static const String locationPin = 'assets/icons/location-pin.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String close = 'assets/icons/close-circle.svg';
+  static const String people = 'assets/icons/people-1.svg';
+  static const String filledPeople = 'assets/icons/people-2.svg';
+}
