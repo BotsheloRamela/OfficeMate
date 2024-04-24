b3da66f9ece034a3ae6a5a424b50bc1d834da907
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-04-24 23:15:21 +0200

    Create Home Screen implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:office_mate/firebase_options.dart';
+import 'package:office_mate/ui/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,11 +19,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/ui/screens/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:office_mate/ui/widgets/office_card.dart';
+import 'package:office_mate/utils/constants.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(AppConstants.horizontalAppPadding),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 40.0),
+              Text(
+                'All Offices',
+                style: TextStyle(
+                  fontSize: AppConstants.xlFontSize,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              SizedBox(height: 20.0),
+              OfficeCard(
+                companyName: 'Google',
+                occupantsCount: 5,
+                officeCapacity: 10,
+                location: 'Mountain View, CA',
+                officeColor: '0xFF4285F4',
+                email: 'info@google.com',
+                phone: '1-800-123-4567',
+              )
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
