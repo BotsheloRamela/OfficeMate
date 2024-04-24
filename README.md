733b995c0c4ebe37a54e38b2be858b1000580f19
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-04-24 17:53:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# OfficeMate
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/botsheloramela/office_mate/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.botsheloramela.office_mate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
