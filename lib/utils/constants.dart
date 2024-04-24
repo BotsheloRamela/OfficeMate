ae5476920179a4b08a8e7d259be2261b91099962
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-04-24 22:54:54 +0200

    Create app constants util class

---
+++ lib/utils/constants.dart
@@ -0,0 +1,12 @@
+class AppConstants {
+  static const double defaultBorderRadius = 15.0;
+  static const double textFieldBorderRadius = 15.0;
+  static const double horizontalAppPadding = 20.0;
+  static const double xsFontSize = 12.0;
+  static const double smFontSize = 14.0;
+  static const double mdFontSize = 16.0;
+  static const double lgFontSize = 18.0;
+  static const double xlFontSize = 20.0;
+  static const double lgButtonHeight = 55.0;
+  static const double smButtonHeight = 40.0;
+}
