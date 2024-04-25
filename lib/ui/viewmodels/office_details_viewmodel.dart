bcf96e28d30d3b9ebe9445b5ba82078f9de6dcd7
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-04-25 22:14:09 +0200

    Add updateWorker method in OfficeDetailsViewModel

--- lib/ui/viewmodels/office_details_viewmodel.dart
+++ lib/ui/viewmodels/office_details_viewmodel.dart
@@ -10,6 +10,7 @@ class OfficeDetailsViewModel extends ChangeNotifier {
 
   final FirebaseService _firebaseService = FirebaseService();
 
+  /// Method to create a new worker
   Future<void> createWorker(
     String firstName, 
     String lastName, 
@@ -18,25 +19,51 @@ class OfficeDetailsViewModel extends ChangeNotifier {
   ) async {
     try {
 
-      String uuid = const Uuid().v4();
-
       OfficeWorker officeWorker = OfficeWorker(
         name: firstName,
         familyName: lastName,
         officeId: officeId,
         avatarId: avatarId,
-        workerId: uuid
+        workerId:  const Uuid().v4()
       );
 
       bool success = await _firebaseService.createWorker(officeWorker);
 
       if (!success) {
-
+        // TODO: Handle error
       }
     } catch (e) {
       log.e('Error creating worker: $e');
       rethrow;
     }
   }
-  
+
+  /// Method to update an existing worker
+  Future<void> updateWorker(
+    String firstName, 
+    String lastName, 
+    String officeId, 
+    String avatarId,
+    String workerId
+  ) async {
+    try {
+      OfficeWorker officeWorker = OfficeWorker(
+        name: firstName,
+        familyName: lastName,
+        officeId: officeId,
+        avatarId: avatarId,
+        workerId: workerId
+      );
+
+      bool success = await _firebaseService.updateWorker(officeWorker);
+
+      if (!success) {
+        // TODO: Handle error
+      }
+    } catch (e) {
+      log.e('Error updating worker: $e');
+      rethrow;
+    }
+  }
+
 }
