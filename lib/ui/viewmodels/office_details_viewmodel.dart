7a06497f16d2a126a8edebd0f784f0fd244b1c6b
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-04-25 17:57:31 +0200

    Implement workerId generation in OfficeDetailsViewModel

--- lib/ui/viewmodels/office_details_viewmodel.dart
+++ lib/ui/viewmodels/office_details_viewmodel.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:office_mate/data/models/office_worker.dart';
 import 'package:office_mate/data/services/firebase_service.dart';
 import 'package:office_mate/utils/logging.dart';
+import 'package:uuid/uuid.dart';
 
 class OfficeDetailsViewModel extends ChangeNotifier {
   final log = logger;
@@ -16,17 +17,21 @@ class OfficeDetailsViewModel extends ChangeNotifier {
     String avatarId
   ) async {
     try {
+
+      String uuid = const Uuid().v4();
+
       OfficeWorker officeWorker = OfficeWorker(
         name: firstName,
         familyName: lastName,
         officeId: officeId,
-        avatarId: avatarId
+        avatarId: avatarId,
+        workerId: uuid
       );
 
       bool success = await _firebaseService.createWorker(officeWorker);
 
       if (!success) {
-        
+
       }
     } catch (e) {
       log.e('Error creating worker: $e');
