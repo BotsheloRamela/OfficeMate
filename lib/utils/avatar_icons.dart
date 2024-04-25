93738de909f1e4f6e2fd1ea5c5a7f9188385e4e2
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-04-25 11:07:51 +0200

    Add avatar icon assets

---
+++ lib/utils/avatar_icons.dart
@@ -0,0 +1,10 @@
+
+class AvatarIcons {
+  static const String avatar1 = 'assets/avatars/avatar_1.jpg';
+  static const String avatar2 = 'assets/avatars/avatar_2.jpg';
+  static const String avatar3 = 'assets/avatars/avatar_3.jpg';
+  static const String avatar4 = 'assets/avatars/avatar_4.jpg';
+  static const String avatar5 = 'assets/avatars/avatar_5.jpg';
+  static const String avatar6 = 'assets/avatars/avatar_6.jpg';
+  static const String avatar7 = 'assets/avatars/avatar_7.jpg';
+}
