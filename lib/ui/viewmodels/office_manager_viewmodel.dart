a9b2d3d7f44a7b7c815b4f45a8e3065a916a8a2e
Botshelo Ramela <ramelabotshelo12@gmail.com>
2024-04-26 18:40:38 +0200

    Refactor OfficeManagerViewModel

--- lib/ui/viewmodels/office_manager_viewmodel.dart
+++ lib/ui/viewmodels/office_manager_viewmodel.dart
@@ -11,6 +11,16 @@ class OfficeManagerViewModel extends ChangeNotifier{
 
   final FirebaseService _firebaseService = FirebaseService();
 
+  List<Office> offices = [];
+  bool _isLoading = false;
+  Map<String, int> officeWorkerCount = {};
+
+  // Getters for offices and officeWorkerCount
+  List<Office> get getOffices => offices;
+  bool get isLoading => _isLoading;
+  Map<String, int> get getOfficeWorkerCount => officeWorkerCount;
+
+
   /// Method to create an office
   Future<void> createOffice(
     String name,
@@ -32,13 +42,16 @@ class OfficeManagerViewModel extends ChangeNotifier{
         officeId: const Uuid().v4()
       );
 
-      await _firebaseService.createOffice(office);
+      await _firebaseService.createOffice(office).then((value) => 
+        offices.add(office),
+      );
       notifyListeners();
     } catch (e) {
       log.e('Error creating office: $e');
       notifyListeners();
     }
   }
+
 
   /// Method to update an office
   Future<void> updateOffice(
@@ -61,7 +74,9 @@ class OfficeManagerViewModel extends ChangeNotifier{
         officeId: officeId
       );
 
-      await _firebaseService.updateOffice(office);
+      await _firebaseService.updateOffice(office).then((value) => 
+        offices[offices.indexWhere((element) => element.officeId == officeId)] = office
+      );
       notifyListeners();
     } catch (e) {
       log.e('Error updating office: $e');
@@ -69,18 +84,60 @@ class OfficeManagerViewModel extends ChangeNotifier{
     }
   }
 
+
+  /// Method to fetch offices
+  Future<void> fetchOffices() async {
+    try {
+      _isLoading = true;
+      //notifyListeners();
+
+      offices = await _firebaseService.getOffices();
+
+      // Iterate through the list of offices to get the worker count for each office
+      for (Office office in offices) {
+        officeWorkerCount[office.officeId] = await _getOfficeWorkerCount(office.officeId);
+      }
+
+      _isLoading = false;
+      notifyListeners();
+    } catch (e) {
+      _isLoading = false;
+      notifyListeners();
+      log.e('Error fetching offices: $e');
+      rethrow;
+    }
+  }
+
+
+  /// Method to delete an office
   deleteOffice(String officeId) {
     _deleteOffice(officeId);
   }
 
-  /// Method to delete an office
+
   Future<void> _deleteOffice(String officeId) async {
     try {
-      await _firebaseService.deleteOffice(officeId);
+      await _firebaseService.deleteOffice(officeId).then((value) => 
+        offices.removeWhere((element) => element.officeId == officeId)
+      );
       notifyListeners();
     } catch (e) {
       log.e('Error deleting office: $e');
       notifyListeners();
+    }
+  }
+
+
+  /// Method to get count of workers in an office
+  Future<int> _getOfficeWorkerCount(String officeId) async {
+    try {
+      int count = await _firebaseService.getWorkerCountForOffice(officeId);
+      notifyListeners();
+      return count;
+    } catch (e) {
+      notifyListeners();
+      log.e('Error fetching office worker count: $e');
+      rethrow;
     }
   }
 }
